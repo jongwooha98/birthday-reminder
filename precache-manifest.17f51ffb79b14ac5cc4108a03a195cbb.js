43457d9d9332fed105c396be94424c4e0bf9ee55
JongWoo Ha <jongwooha98@gmail.com>
2021-07-16 20:29:05 +0900

    Updates

---
+++ precache-manifest.17f51ffb79b14ac5cc4108a03a195cbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b603029f4e1dd700dec47a9c8ec66a15",
+    "url": "/birthday-reminder/index.html"
+  },
+  {
+    "revision": "dd52eca86cc3fff70fc5",
+    "url": "/birthday-reminder/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "eb9e5e52d0fbc598da4b",
+    "url": "/birthday-reminder/static/css/main.b8756d21.chunk.css"
+  },
+  {
+    "revision": "dd52eca86cc3fff70fc5",
+    "url": "/birthday-reminder/static/js/2.47f5966e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday-reminder/static/js/2.47f5966e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb9e5e52d0fbc598da4b",
+    "url": "/birthday-reminder/static/js/main.18ee81ee.chunk.js"
+  },
+  {
+    "revision": "a1ff553ac3d675eebff3",
+    "url": "/birthday-reminder/static/js/runtime-main.ed31b697.js"
+  }
+]);
